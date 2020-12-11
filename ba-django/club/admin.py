d6ceb74d1116d6d0c145dfe3e8ad567995ca6867
ryhuz <shawn.wee90@hotmail.com>
2020-12-11 16:26:58 +0800

    added to admin view

--- ba-django/club/admin.py
+++ ba-django/club/admin.py
@@ -4,6 +4,10 @@ from .models import Club, User_Details, Workout
 # Register your models here.
 
 admin.site.register(Club)
-admin.site.register(User_Details)
+admin.site.register(UserDetail)
 admin.site.register(Workout)
+admin.site.register(WorkoutResult)
+admin.site.register(WorkoutTemplate)
+admin.site.register(SavedWorkouts)
+admin.site.register(TrackedAthletes)
 # remember to import model
