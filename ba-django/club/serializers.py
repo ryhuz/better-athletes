861eb7dd181ed91bd9d883951860d6c53e2d9b6a
stralth <kinken1992@hotmail.com>
2020-12-11 21:58:39 +0800

    tweaked signup to enable User and UserDetail save

--- ba-django/club/serializers.py
+++ ba-django/club/serializers.py
@@ -1,11 +1,12 @@
 from rest_framework import serializers
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
 from django.contrib.auth.models import User
+from club.models import Club
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('email', 'username', 'password')
+        fields = "__all__"
 
     def create(self, validated_data):
         password = validated_data.pop('password', None)
@@ -14,3 +15,8 @@ class UserSerializer(serializers.ModelSerializer):
             instance.set_password(password)
         instance.save()
         return instance
+
+class ClubSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Club
+        fields = ('club_name', 'club_desc')
