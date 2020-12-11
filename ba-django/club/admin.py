d4491b1f6908002fe18bc0a27b85b81a63d7f213
ryhuz <shawn.wee90@hotmail.com>
2020-12-11 16:37:06 +0800

    refined models

--- ba-django/club/admin.py
+++ ba-django/club/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Club, User_Details, Workout
+from .models import Club, UserDetail, Workout, WorkoutResult, WorkoutTemplate, SavedWorkout, TrackedAthlete
 
 # Register your models here.
 
@@ -8,6 +8,6 @@ admin.site.register(UserDetail)
 admin.site.register(Workout)
 admin.site.register(WorkoutResult)
 admin.site.register(WorkoutTemplate)
-admin.site.register(SavedWorkouts)
-admin.site.register(TrackedAthletes)
+admin.site.register(SavedWorkout)
+admin.site.register(TrackedAthlete)
 # remember to import model
--- ba-django/club/models.py
+++ ba-django/club/models.py
@@ -32,64 +32,6 @@ class UserDetail(models.Model):
     def __str__(self):
         return self.base_user
 
-class Workout(models.Model):
-    workout_name = models.CharField(max_length=50)
-    exercise = ArrayField( # number of sets
-        ArrayField( # exercises
-            models.CharField(max_length=50, blank=True)
-        )
-    )
-    reps = ArrayField( # number of sets
-        ArrayField( # reps per exercise
-            models.IntegerField(validators=[MinValueValidator(1)], blank=True)
-        )
-    )
-    targets = ArrayField( # number of sets
-        ArrayField( # target per exercise
-            models.CharField(max_length=50, blank=True)
-        )
-    )
-    created_date = models.DateTimeField(auto_now=True)
-    workout_date = models.DateField()
-    based_on_template = models.ForeignKey(WorkoutTemplate, related_name=("workout"), on_delete=models.PROTECT)
-
-    def __str__(self):
-        return self.workout_name
-
-class WorkoutResult(models.Model):
-    athlete = models.ForeignKey(User, related_name=("athlete"), on_delete=models.PROTECT)
-    workout = models.ForeignKey(Workout, related_name=("athlete"), on_delete=models.PROTECT)
-    completed = models.BooleanField(default=False)
-    results = ArrayField(   # number of sets
-        ArrayField(         # results per exercise
-            models.CharField(max_length=10, blank=True)
-        )
-    )
-
-    UNIT = [
-        ('sec', 's'),
-        ('min', 'min'),
-        ('m', 'meters'),
-        ('kg', 'kg'),
-        ('-', ''),
-    ]
-    units = ArrayField(     # number of sets
-        ArrayField(         # units per result
-            models.CharField(max_length=3, choices=GENDER, default="")
-        )
-    )
-    comments = ArrayField(  # number of sets
-        ArrayField(         # comments per exercise
-            models.CharField(max_length=255, blank=True)
-        )
-    )
-    completed_on = models.DateTimeField(auto_now=True)
-    # workout_conversation = 
-    # performance_tracking
-
-    def __str__(self):
-        return self.athlete + self.workout + self.completed_on
-
 class WorkoutTemplate(models.Model):
     workout_name = models.CharField(max_length=50)
     exercise = ArrayField( # number of sets
@@ -111,12 +53,72 @@ class WorkoutTemplate(models.Model):
     def __str__(self):
         return self.workout_name + " template"
 
-class SavedWorkouts(models.Model):
-    saved_workouts = models.ManyToManyField(Workout, related_name("saved_workouts"))
-    saved_templates = models.ManyToManyField(WorkoutTemplate, related_name("saved_templates"))
+class Workout(models.Model):
+    workout_name = models.CharField(max_length=50)
+    exercise = ArrayField( # number of sets
+        ArrayField( # exercises
+            models.CharField(max_length=50, blank=True)
+        )
+    )
+    reps = ArrayField( # number of sets
+        ArrayField( # reps per exercise
+            models.IntegerField(validators=[MinValueValidator(1)], blank=True)
+        )
+    )
+    targets = ArrayField( # number of sets
+        ArrayField( # target per exercise
+            models.CharField(max_length=50, blank=True)
+        )
+    )
+    created_date = models.DateTimeField(auto_now=True)
+    workout_date = models.DateField()
+    based_on_template = models.ForeignKey(WorkoutTemplate, related_name=("workout"), on_delete=models.PROTECT, null=True)
+
+    def __str__(self):
+        return self.workout_name
+
+class WorkoutResult(models.Model):
+    athlete = models.ForeignKey(User, related_name=("athlete"), on_delete=models.PROTECT)
+    workout = models.ForeignKey(Workout, related_name=("athlete"), on_delete=models.PROTECT)
+    completed = models.BooleanField(default=False)
+    results = ArrayField(   # number of sets
+        ArrayField(         # number of exercises
+            ArrayField(         # reps per exercise
+                models.CharField(max_length=10, blank=True)
+            )
+        )
+    )
+
+    UNIT = [
+        ('sec', 's'),
+        ('min', 'min'),
+        ('m', 'meters'),
+        ('kg', 'kg'),
+        ('-', ''),
+    ]
+    units = ArrayField(     # number of sets
+        ArrayField(         # units per result
+            models.CharField(max_length=3, choices=UNIT, default="")
+        )
+    )
+    comments = ArrayField(  # number of sets
+        ArrayField(         # comments per exercise
+            models.CharField(max_length=255, blank=True)
+        )
+    )
+    completed_on = models.DateTimeField(auto_now=True)
+    # workout_conversation = 
+    # performance_tracking
+
+    def __str__(self):
+        return self.athlete + self.workout + self.completed_on
+
+class SavedWorkout(models.Model):
+    saved_workouts = models.ManyToManyField(Workout)
+    saved_templates = models.ManyToManyField(WorkoutTemplate)
     saved_by = models.ForeignKey(User, related_name=("saved"), on_delete=models.CASCADE)
 
-class TrackedAthletes(models.Model):
-    tracked_athletes = models.ManyToManyField(User, related_name=("tracked_by"))
+class TrackedAthlete(models.Model):
+    tracked_athletes = models.ManyToManyField(User)
     notes = models.TextField()
     tracked_by = models.ForeignKey(User, related_name=("tracking"), on_delete=models.CASCADE)
