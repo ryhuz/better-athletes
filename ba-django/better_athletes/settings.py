1bb353a6527a471bb9aecc715132cf0d203fc12b
stralth <kinken1992@hotmail.com>
2020-12-13 14:19:08 +0800

    basic login/signup done

--- ba-django/better_athletes/settings.py
+++ ba-django/better_athletes/settings.py
@@ -31,6 +31,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ORIGIN_ALLOW_ALL = False
+CORS_ORIGIN_WHITELIST = (
+       'http://localhost:3000',
+)
+
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
@@ -45,6 +50,7 @@ REST_FRAMEWORK = {
 INSTALLED_APPS = [
     'club',
     'rest_framework',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,7 +60,7 @@ INSTALLED_APPS = [
 ]
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=14),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': False,
@@ -69,6 +75,7 @@ SIMPLE_JWT = {
 }
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
--- ba-react/src/Components/Routes/AllRoutes.jsx
+++ ba-react/src/Components/Routes/AllRoutes.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import React from 'react'
 import LandingPage from "./LandingPage/LandingPage"
 import BetterAthletes from "./Private/BetterAthletes"
+import Login from "./Auth/Login"
+import Register from "./Auth/Register"
 
 function AllRoutes() {
     return (
@@ -9,6 +11,12 @@ function AllRoutes() {
         <Switch>
           <Route path="/" exact>
             <LandingPage/>
+          </Route>
+          <Route path="/login">
+            <Login/>
+          </Route>
+          <Route path="/register">
+            <Register/>
           </Route>
           <BetterAthletes />
         </Switch>
