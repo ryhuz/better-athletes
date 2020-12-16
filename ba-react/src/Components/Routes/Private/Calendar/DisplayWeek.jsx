56dce27c9c49b6aad3d2c3b83e08618169e7ac42
ryhuz <shawn.wee90@hotmail.com>
2020-12-16 23:57:16 +0800

    calendar faded bg

--- ba-react/src/Components/Routes/Private/Calendar/DisplayWeek.jsx
+++ ba-react/src/Components/Routes/Private/Calendar/DisplayWeek.jsx
@@ -9,15 +9,22 @@ function DisplayWeek({ week, workouts, relative }) {
             {workouts ?
                 <>
                     {week.map(day => (
-                        <Col key={day} className="border overflow-auto cal-day">
-                            <span className={`${(day.format('dd') === "Su" || day.format('dd') === "Sa") && 'text-danger'}`}>{day.format("Do")}</span>
-                            {workouts.map((x, i) => (
-                                <NavLink to={`/betterathletes/view_workout/${x.workout_id}`} className="red-shadow">
-                                    {day.format('YYYY-MM-DD') === x.workout_date &&
-                                        <div key={i} id={x.workout_id} className="ml-3 display-6">{x.workout_name}</div>}
-                                </NavLink>
-                            ))}
-                        </Col>
+                        <>
+                            <Col key={day} className={`border overflow-auto cal-day`}>
+                                <span className={`${(day.format('dd') === "Su" || day.format('dd') === "Sa") && 'text-danger'}`}>{day.format("Do")}</span>
+                                {workouts.map((x, i) => (
+                                    <>
+                                        {day.format('YYYY-MM-DD') === x.workout_date &&
+                                            <NavLink to={`/betterathletes/view_workout/${x.workout_id}`} className="red-shadow">
+
+                                                <div key={i} id={x.workout_id} className="ml-3 display-6">{x.workout_name}</div>
+                                            </NavLink>
+                                        }
+                                    </>
+                                ))}
+                                <div className={`${relative === "prev" ? "cal-gradient-prev" : ""} ${relative === "next" ? "cal-gradient-next" : ""}`}></div>
+                            </Col>
+                        </>
                     ))}
                 </> :
                 <div>
