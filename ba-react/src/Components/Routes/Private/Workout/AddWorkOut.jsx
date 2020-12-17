46de743315d4b1a82f858d074515ff11f11ef928
ryhuz <shawn.wee90@hotmail.com>
2020-12-17 19:34:01 +0800

    validation added

--- ba-react/src/Components/Routes/Private/Workout/AddWorkOut.jsx
+++ ba-react/src/Components/Routes/Private/Workout/AddWorkOut.jsx
@@ -2,11 +2,14 @@ import React, { useState, useEffect } from 'react'
 import Select from 'react-select';
 import makeAnimated from 'react-select/animated';
 import DatePicker from 'react-modern-calendar-datepicker';
-import { Col, Row, Form, Button, Container } from "react-bootstrap";
+import { Col, Row, Form, Button, Container, Modal } from "react-bootstrap";
 import axios from "axios";
 import { Redirect } from 'react-router-dom';
 
 function WorkOut({ isAuth }) {
+    const [showEmpty, setShowEmpty] = useState(false);
+    const [showError, setShowError] = useState(false);
+    const [saving, setSaving] = useState(false);
     const [selectedOption, setSelectedOption] = useState(null);
     const [isSubmit, setisSubmit] = useState(false);
     const [athletes, setAthletes] = useState([{
@@ -33,6 +36,10 @@ function WorkOut({ isAuth }) {
         })
 
     const animatedComponents = makeAnimated();
+    const handleClose = () => setShowEmpty(false);
+    const handleShow = () => setShowEmpty(true);
+    const handleCloseError = () => setShowError(false);
+    const handleShowError = () => setShowError(true);
 
     function AddSet(i, item = [{
         exercise: "",
@@ -119,123 +126,134 @@ function WorkOut({ isAuth }) {
         setForm(temp)
     }
 
-    // =========================== TO UPDATE AXIOS ONCE API ROUTES FINALISED ================================\\
+    function anyEmptyInputs() {
+        let allInputs = document.querySelectorAll('input');
+        let any = false
+        allInputs.forEach(x => {
+            if (!x.value) {
+                x.classList.add('empty');
+                x.addEventListener('click', () => x.classList.remove('empty'));
+                any = true;
+            }
+        })
+        return any;
+    }
+
     /**
      * @POST = send data from INPUT FORM STATE TO DJANGO DB
      * @reminder = to check data format, convert obj to array to send to back using another set state for submitworkout func
      */
     async function submitWorkout() {
-        let maxLength = 0;
-        inputForm.sets.forEach(set => {
-            if (set.length > maxLength) maxLength = set.length
-        })
-        let djangoFormVersion = {
-            athletes: selectedOption,
-            workout_name: inputForm.workout_name,
-            workout_date: "",
-            exercises: [],
-            reps: [],
-            rests: [],
-            targets: [],
-            units: [],
-            comments: [],
-            results: []
-        }
-
-        if (date === null) {
-            djangoFormVersion.workout_date = ""
+        if (anyEmptyInputs()) {
+            handleShow()
         } else {
-            let formated_date = date.year + "-" + date.month + "-" + date.day
-            djangoFormVersion.workout_date = formated_date
-        }
-        let max_rectangular_len = 0
-        
-        /* rectangularisation of every key except results (
-         for results, please refer to separate loop, due to setting the max_rectangular_length
-         in the first loop) 
-        */
-        inputForm.sets.forEach(set => {
-            let exerciseSet = []
-            let repSet = []
-            let restSet = []
-            let targetsSet = []
-            let unitsSet = []
-            let commentsSet = []           
-
-            set.forEach(ex => {
-                exerciseSet.push(ex.exercise)
-                repSet.push(ex.reps)
-                restSet.push(ex.rests)
-                targetsSet.push(ex.targets)
-                unitsSet.push(ex.units)
-                commentsSet.push(ex.comments)
-
-                if (Number(ex.reps) > max_rectangular_len){
-                    max_rectangular_len = Number(ex.reps)
-                }
-                
+            setSaving(true)
+            let maxLength = 0;
+            inputForm.sets.forEach(set => {
+                if (set.length > maxLength) maxLength = set.length
             })
-
-            console.log(max_rectangular_len)
-            /* Rectangularising data */
-            if (exerciseSet.length < maxLength) {
-                for (let i = 0; i <= maxLength - exerciseSet.length; i++) {
-                    exerciseSet.push("")
-                    repSet.push("")
-                    restSet.push("")
-                    targetsSet.push("")
-                    unitsSet.push("")
-                    commentsSet.push("")
-                }
+            let djangoFormVersion = {
+                athletes: selectedOption,
+                workout_name: inputForm.workout_name,
+                workout_date: "",
+                exercises: [],
+                reps: [],
+                rests: [],
+                targets: [],
+                units: [],
+                comments: [],
+                results: []
             }
-            djangoFormVersion.exercises.push(exerciseSet)
-            djangoFormVersion.reps.push(repSet)
-            djangoFormVersion.rests.push(restSet)
-            djangoFormVersion.targets.push(targetsSet)
-            djangoFormVersion.units.push(unitsSet)
-            djangoFormVersion.comments.push(commentsSet)
-            // console.log(djangoFormVersion)
-        })
-        
-        /** Rectangularisation of results based on number of reps captured
-         *  (for all others, please refer to above loop)
-         */
 
-        inputForm.sets.forEach(set => {
-            let resultsSet = []
-            set.forEach(ex => {
-                resultsSet.push(new Array(max_rectangular_len).fill(""))               
+            if (date === null) {
+                djangoFormVersion.workout_date = ""
+            } else {
+                let formated_date = date.year + "-" + date.month + "-" + date.day
+                djangoFormVersion.workout_date = formated_date
+            }
+            let max_rectangular_len = 0
+
+            /* rectangularisation of every key except results (
+             for results, please refer to separate loop, due to setting the max_rectangular_length
+             in the first loop) 
+            */
+            inputForm.sets.forEach(set => {
+                let exerciseSet = []
+                let repSet = []
+                let restSet = []
+                let targetsSet = []
+                let unitsSet = []
+                let commentsSet = []
+
+                set.forEach(ex => {
+                    exerciseSet.push(ex.exercise)
+                    repSet.push(ex.reps)
+                    restSet.push(ex.rests)
+                    targetsSet.push(ex.targets)
+                    unitsSet.push(ex.units)
+                    commentsSet.push(ex.comments)
+
+                    if (Number(ex.reps) > max_rectangular_len) {
+                        max_rectangular_len = Number(ex.reps)
+                    }
+
+                })
+
+                /* Rectangularising data */
+                if (exerciseSet.length < maxLength) {
+                    for (let i = 0; i <= maxLength - exerciseSet.length; i++) {
+                        exerciseSet.push("")
+                        repSet.push("")
+                        restSet.push("")
+                        targetsSet.push("")
+                        unitsSet.push("")
+                        commentsSet.push("")
+                    }
+                }
+                djangoFormVersion.exercises.push(exerciseSet)
+                djangoFormVersion.reps.push(repSet)
+                djangoFormVersion.rests.push(restSet)
+                djangoFormVersion.targets.push(targetsSet)
+                djangoFormVersion.units.push(unitsSet)
+                djangoFormVersion.comments.push(commentsSet)
             })
-            console.log(max_rectangular_len)
-            /* Rectangularising data */
-            if (resultsSet.length < maxLength) {
-                for (let i = 0; i <= maxLength - resultsSet.length; i++) {
+
+            /** Rectangularisation of results based on number of reps captured
+             *  (for all others, please refer to above loop)
+             */
+
+            inputForm.sets.forEach(set => {
+                let resultsSet = []
+                set.forEach(ex => {
                     resultsSet.push(new Array(max_rectangular_len).fill(""))
+                })
+                /* Rectangularising data */
+                if (resultsSet.length < maxLength) {
+                    for (let i = 0; i <= maxLength - resultsSet.length; i++) {
+                        resultsSet.push(new Array(max_rectangular_len).fill(""))
+                    }
                 }
-            }
-            djangoFormVersion.results.push(resultsSet)
-            // console.log(djangoFormVersion)
-        })
-        console.log(djangoFormVersion)
-
-        try {
-            let response = await axios.post("http://localhost:8000/api/workouts", djangoFormVersion, {
-                headers: {
-                    'Authorization': "JWT " + localStorage.getItem('token'),
-                    'Content-Type': 'application/json',
-                    'accept': "application/json"
-                }
+                djangoFormVersion.results.push(resultsSet)
             })
-            if (response) {
-                // setisSubmit(true);
-            }
-        } catch (error) {
-            console.log(error)
-            // set error state to whatever and show modal
-        }
 
+            try {
+                let response = await axios.post("http://localhost:8000/api/workouts", djangoFormVersion, {
+                    headers: {
+                        'Authorization': "JWT " + localStorage.getItem('token'),
+                        'Content-Type': 'application/json',
+                        'accept': "application/json"
+                    }
+                })
+                if (response) {
+                    setisSubmit(true);
+                }
+            } catch (error) {
+                handleShowError()
+                setSaving(false)
+            }
+        }
     }
-    // console.log(inputForm)
+
     /**
      * @GET = retrieve Athlete data and populate in drop down list
      */
@@ -258,10 +276,10 @@ function WorkOut({ isAuth }) {
         }
         getAthletes();
         /* Get date from URL */
-        if(window.location.search){
+        if (window.location.search) {
             let params = window.location.search.split('?').slice(1)
             let dateFromURL = {}
-            params.forEach(x=>{
+            params.forEach(x => {
                 let temp = x.split('=')
                 dateFromURL[temp[0]] = Number(temp[1])
             })
@@ -283,7 +301,7 @@ function WorkOut({ isAuth }) {
                 <Col md={12} className="outer_form">
                     <Form>
                         <Row className="no-gutters">
-                            {/* Workout Name */}                            
+                            {/* Workout Name */}
                             <Col md={6} className="mx-4">
                                 <Form.Control
                                     name="workout_name"
@@ -300,7 +318,7 @@ function WorkOut({ isAuth }) {
                                     inputPlaceholder="Workout Day"
                                     shouldHighlightWeekends
                                     colorPrimary="#000000"
-                                    calendarPopperPosition = "bottom"
+                                    calendarPopperPosition="bottom"
                                 />
                             </Col>
                             {/* Select Athlete to assign */}
@@ -411,26 +429,46 @@ function WorkOut({ isAuth }) {
                                         <Col md={3}>
                                             <Button block size='sm'
                                                 onClick={() => AddSet(index, item)}
-                                                variant="secondary"><b>Repeat Set</b></Button>                                        </Col>
+                                                variant="secondary"><b>Repeat Set</b></Button>
+                                        </Col>
                                     </Row>
                                 </Form.Group>
                             </div>
                         ))}
                     </Form>
-
-                    <Row >
+                    <Row>
                         <Col md={12} className="d-flex flex-row-reverse">
-                            <Button
-                                variant="warning"
-                                type="submit"
-                                onClick={submitWorkout}
-                            >Save Exercise</Button>
+                            <Button variant="warning" type="submit" onClick={submitWorkout} disabled={saving}>
+                                    {saving ? "Saving..." : "Save Exercise" }
+                            </Button>
                         </Col>
                     </Row>
                 </Col>
-
-
             </Row>
+            {/* Any empty fields modal */}
+            <Modal show={showEmpty} onHide={handleClose} centered >
+                <Modal.Header className="bg-dark">
+                    <Modal.Title>Oops</Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="bg-dark bigger-text2">Please make sure you fill in all fields before saving the workout</Modal.Body>
+                <Modal.Footer className="bg-dark">
+                    <Button variant="main" onClick={handleClose}>
+                        Ok
+                    </Button>
+                </Modal.Footer>
+            </Modal>
+            {/* Registration error */}
+            <Modal show={showError} onHide={handleCloseError} centered >
+                <Modal.Header className="bg-dark">
+                    <Modal.Title>Something went wrong</Modal.Title>
+                </Modal.Header>
+                <Modal.Body className="bg-dark bigger-text2">We could not save this workout</Modal.Body>
+                <Modal.Footer className="bg-dark">
+                    <Button variant="main" onClick={handleCloseError}>
+                        Ok
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </Container>
     )
 }
